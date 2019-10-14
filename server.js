bc69d83f0144732604fe8b8fdd54bf9bad542610
soheila <soheilalotfi@ymail.com>
2019-10-14 13:48:29 -0400

    add saved property to article.js model

--- server.js
+++ server.js
@@ -53,26 +53,6 @@ app.get("/", function (req, res) {
             result.summary = $(this)
                 .find("p")
                 .text();
-            // axios.get("http://www.echojs.com/").then(function (response) {
-            //     // Then, we load that into cheerio and save it to $ for a shorthand selector
-            //     var $ = cheerio.load(response.data);
-
-            //     // Now, we grab every h2 within an article tag, and do the following:
-            //     $("article h2").each(function (i, element) {
-            //         // Save an empty result object
-            //         var result = {};
-
-            //         // Add the text and href of every link, and save them as properties of the result object
-            //         result.title = $(this)
-            //             .children("a")
-            //             .text();
-            //         result.link = $(this)
-            //             .children("a")
-            //             .attr("href");
-            //         result.summary = $(this)
-            //             .children("a")
-            //             .attr("href");
-
 
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
@@ -99,6 +79,17 @@ app.get("/articles", function (req, res) {
         res.json(err);
     })
 });
+
+app.get("/saved", function (req, res) {
+
+    db.Article.find({}).then(function (data) {
+        res.json(data);
+    }).catch(function (err) {
+        res.json(err);
+    })
+});
+
+
 
 
 
