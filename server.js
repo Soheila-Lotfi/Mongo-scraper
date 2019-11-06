a2ac4c7b220c6bc3f7d44cd9bbf9965967a8d7bf
soheila <soheilalotfi@ymail.com>
2019-11-05 20:09:37 -0500

    port config

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3026;
+var PORT = process.env.PORT || 3026;
 
 // Initialize Express
 var app = express();
